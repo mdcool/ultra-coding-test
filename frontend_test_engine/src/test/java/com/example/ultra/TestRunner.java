7f37648d27e8d566d7317171811b6c29dc88f3e5
Denis Mardari <mdcoolcomrat@gmail.com>
2022-03-10 16:35:19 +0200

    Added HTML report plugin

--- frontend_test_engine/src/test/java/com/example/ultra/TestRunner.java
+++ frontend_test_engine/src/test/java/com/example/ultra/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/resources/features",
-//        plugin = {"pretty", "html:build/cucumber/pretty"},
+        plugin = {"pretty", "html:build/cucumber/pretty"},
         glue = {"com.example.ultra"}
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
